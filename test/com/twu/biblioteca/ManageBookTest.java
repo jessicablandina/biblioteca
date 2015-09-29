00e215e8f24000c312d645229a4b143fa350d76e
jessicablandina <jessicablandina84@gmail.com>
2015-09-29 10:55:25 -0400

    Removes @before on managebooktest

--- test/com/twu/biblioteca/ManageBookTest.java
+++ test/com/twu/biblioteca/ManageBookTest.java
@@ -1,39 +1,34 @@
 package com.twu.biblioteca;
 
-import junit.framework.TestCase;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class ManageBookTest extends TestCase {
-
+public class ManageBookTest{
 
     private Book book;
     private Library library;
-
-    @Before
-    public void setUp(){
-        library = new Library();
-    }
 
     @Test
     public void shouldAddABookWithAllValues(){
         String author = "John Doe";
         String title = "An Amazing book";
-        int numberOfPages = 190;
         String editor = "Editor Me";
         String isbn = "123.AB23";
 
-        book = new Book(title, author,numberOfPages, editor, isbn);
+        book = new Book(title, author, editor, isbn);
+        library = new Library();
         library.addBook(book);
 
         assertThat(book.getTitle(), is(title));
         assertThat(book.getAuthor(), is(author));
-        assertThat(book.getNumberOfPages(), is(numberOfPages));
         assertThat(book.getEditor(), is(editor));
         assertThat(book.getIsbn(), is(isbn));
     }
 
+    @Test
+    public void shouldListAllTheBooks(){
+
+    }
 }
